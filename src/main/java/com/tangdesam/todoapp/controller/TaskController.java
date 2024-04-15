0b9908543047ccb65b50fdd126cf0573ea02fe27
ilufa <tangdesam@gmail.com>
2024-04-15 12:52:23 +0700

    Implemented Create New Data Functionality

--- src/main/java/com/tangdesam/todoapp/controller/TaskController.java
+++ src/main/java/com/tangdesam/todoapp/controller/TaskController.java
@@ -1,11 +1,17 @@
 package com.tangdesam.todoapp.controller;
 
 import com.tangdesam.todoapp.dao.TaskDao;
+import com.tangdesam.todoapp.entity.Task;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/task")
@@ -17,6 +23,27 @@ public class TaskController {
     public String list(Model model) {
         model.addAttribute("taskList", taskDao.findAll());
         return("task/list");
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("task", new Task());
+        return("task/create");
+    }
+
+    @PostMapping("/save")
+    public String save(RedirectAttributes redirectAttributes, @Valid Task task, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "task/create";
+        }
+
+        taskDao.save(task);
+
+        redirectAttributes.addFlashAttribute("message", "Task created");
+        redirectAttributes.addFlashAttribute("messageCss", "alert alert-success");
+
+        return("redirect:/task/list");
     }
 
 }
