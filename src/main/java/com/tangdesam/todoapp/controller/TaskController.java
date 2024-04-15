600a561d80eeaa25e8291414f6dada9f9a8a37aa
ilufa <tangdesam@gmail.com>
2024-04-15 13:13:58 +0700

    Implemented Update Data Functionality

--- src/main/java/com/tangdesam/todoapp/controller/TaskController.java
+++ src/main/java/com/tangdesam/todoapp/controller/TaskController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -41,6 +42,36 @@ public class TaskController {
         taskDao.save(task);
 
         redirectAttributes.addFlashAttribute("message", "Task created");
+        redirectAttributes.addFlashAttribute("messageCss", "alert alert-success");
+
+        return("redirect:/task/list");
+    }
+
+    @GetMapping("/edit/{taskId}")
+    public String edit(@PathVariable("taskId") String taskId, Model model, RedirectAttributes redirectAttributes) {
+        Task task = taskDao.findById(taskId).orElse(null);
+
+        if (task == null) {
+            redirectAttributes.addFlashAttribute("message", "Task not found");
+            redirectAttributes.addFlashAttribute("messageCss", "alert alert-danger");
+
+            return("redirect:/task/list");
+        }
+
+        model.addAttribute("task", task);
+        return("task/edit");
+    }
+
+    @PostMapping("/update")
+    public String update(RedirectAttributes redirectAttributes, @Valid Task task, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "task/edit";
+        }
+
+        taskDao.save(task);
+
+        redirectAttributes.addFlashAttribute("message", "Task updated");
         redirectAttributes.addFlashAttribute("messageCss", "alert alert-success");
 
         return("redirect:/task/list");
